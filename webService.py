56cb2016d5a6bcb500693faf2a515d2fcb87909d
cesardlq <cdlq13@hotmail.com>
2015-06-08 18:53:24 -0500

    Analisis

---
+++ webService.py
@@ -0,0 +1,23 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from administradorConsultas import AdministradorConsultas
+from red import xmls_to_red
+
+DIRECTORIO_PROYECTO = '/home/vigtech/shared/'
+
+app = Flask(__name__)
+
+
+@app.route('/red/', methods=['GET'])
+def get_red():
+    red = xmls_to_red([open(DIRECTORIO_PROYECTO+'xml0.xml')], 'RedCoauth')
+    cc=  red.clustering_coefficient()
+    ad = red.average_degree()
+    avs = red.average_strength()
+    apl= red.average_path_lenght()
+    
+    nodos, aristas = red.generar_json()
+    return jsonify({'nodos':nodos, 'aristas':aristas, 'averagePathLenght':apl, 'clusteringCoefficient':cc, 'averageDegree':ad})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
