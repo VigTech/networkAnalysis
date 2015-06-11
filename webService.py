b4d8d28fbddee8002219114bba19fa57e12eb917
cesardlq <cdlq13@hotmail.com>
2015-06-11 14:48:21 -0500

    rebiendo proyecto y usuario

--- webService.py
+++ webService.py
@@ -3,21 +3,23 @@ from flask import Flask, jsonify
 #from administradorConsultas import AdministradorConsultas
 from red import xmls_to_red
 
-DIRECTORIO_PROYECTO = '/home/vigtech/shared/'
+DIRECTORIO_PROYECTO = '/home/vigtech/shared/repository/'
 
 app = Flask(__name__)
 
 
 @app.route('/red/', methods=['GET'])
 def get_red():
-    red = xmls_to_red([open(DIRECTORIO_PROYECTO+'xml0.xml')], 'RedCoauth')
+	proyecto = request.args.get('proyecto')
+	user = request.args.get('user')
+    red = xmls_to_red([open(DIRECTORIO_PROYECTO+'%s.%s/busqueda0.xml'%(usuario,proyecto)], 'RedCoauth')
     cc=  red.clustering_coefficient()
     ad = red.average_degree()
     avs = red.average_strength()
     apl= red.average_path_lenght()
     
     nodos, aristas = red.generar_json()
-    return jsonify({'nodos':nodos, 'aristas':aristas, 'averagePathLenght':apl, 'clusteringCoefficient':cc, 'averageDegree':ad})
+    return jsonify({'nodes':nodos, 'links':aristas, 'averagePathLenght':apl, 'clusteringCoefficient':cc, 'averageDegree':ad})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
