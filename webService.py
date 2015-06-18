5408955786dcb96872b67a73e156fb62a737f853
cesardlq <cdlq13@hotmail.com>
2015-06-18 16:52:21 -0500

    medidas nodos

--- webService.py
+++ webService.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 #from administradorConsultas import AdministradorConsultas
 from red import xmls_to_red
 
@@ -12,14 +12,14 @@ app = Flask(__name__)
 def get_red():
 	proyecto = request.args.get('proyecto')
 	user = request.args.get('user')
-    red = xmls_to_red([open(DIRECTORIO_PROYECTO+'%s.%s/busqueda0.xml'%(usuario,proyecto)], 'RedCoauth')
-    cc=  red.clustering_coefficient()
-    ad = red.average_degree()
-    avs = red.average_strength()
-    apl= red.average_path_lenght()
+	red = xmls_to_red([open(DIRECTORIO_PROYECTO+'%s.%s/busqueda0.xml'%(user,proyecto))], 'RedCoauth')
+	cc=  red.clustering_coefficient()
+	ad = red.average_degree()
+	avs = red.average_strength()
+	apl= red.average_path_lenght()
     
-    nodos, aristas = red.generar_json()
-    return jsonify({'nodes':nodos, 'links':aristas, 'averagePathLenght':apl, 'clusteringCoefficient':cc, 'averageDegree':ad})
+	nodos, aristas = red.generar_json()
+	return jsonify({'nodes':nodos, 'links':aristas, 'averagePathLenght':apl, 'clusteringCoefficient':cc, 'averageDegree':ad})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
